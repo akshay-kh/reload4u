74e3d3bc9c43fbc9c39c23723aeebb415ff3dee9
akshay <akshay@kemuri.in>
2018-08-19 23:31:25 +0530

    Updated Extension     * Added Stop reload feature     * and some refactoring     * fixed a possible Bug in select this tab

--- popup.js
+++ popup.js
@@ -1,20 +1,5 @@
 let selectTab = document.getElementById('selectThisTab');
-
-selectTab.onclick = function(element){
-    function setTabId(tabs) {
-        var currentTab = tabs[0];
-        chrome.storage.sync.set({selectedTab: currentTab.id});
-    }
-var query = { active: true, currentWindow: true };
-    chrome.tabs.query(query, setTabId);
-    chrome.alarms.create('reloadTabAlarm', {delayInMinutes: 0.1, periodInMinutes: 0.2});
-     getTab().then(function(tab){
-        var tabId = tab;
-     }).catch(function(error){
-        alert("something went wrong");
-     });
-    alert("tab stored "+tabId);
-};
+let stopTab = document.getElementById('stopThisTab');
 
 function getTab (){
     return new Promise(function(resolve, reject){
@@ -23,3 +8,29 @@ function getTab (){
     });
     });
 }
+
+selectTab.onclick = function(element){
+    function setTabId(tabs) {
+        var currentTab = tabs[0];
+        chrome.storage.sync.set({selectedTab: currentTab.id});
+    }
+var query = { active: true, currentWindow: true };
+    chrome.tabs.query(query, setTabId);
+    chrome.alarms.create('reloadTabAlarm', {when:Date.now(), periodInMinutes: 0.2});
+     getTab().then(function(tab){
+        var tabId = tab;
+        alert("tab selected");
+     }).catch(function(error){
+        alert("something went wrong");
+     });
+};
+
+stopTab.onclick = function (element) {
+  chrome.alarms.clear('reloadTabAlarm', function(wasCleared) {
+    if(wasCleared) {
+      alert("tab removed from auto reload");
+    } else{
+      alert("failed please try again");
+    }
+  });
+};
